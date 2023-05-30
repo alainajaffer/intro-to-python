612f1cc2a609d773d2100c6937e099b4d90da7e0
Replit user <>
2023-05-30 10:26:07 +0000

    Session 1

--- session_01/exercises_1.py
+++ session_01/exercises_1.py
@@ -8,19 +8,17 @@
 #     - Windows: ```Ctrl + / ```
 #     - Mac: ```Command + /```
 
-
 ## Section A
 # 1. Write code that prints ‘Hello world’.
 
-
+print("Hello world")
 
 # 2. Print the numbers 1 to 5 on a single line.
 
-
+print("1 2 3 4 5")
 
 # 3. Write a script where ‘Hello’ and ‘World’ are printed on two separate lines.
-
-
+print("Hello\nWorld")
 
 # 4. Write a script that prints a list of names, tabbed on separate lines, e.g.
 #     ```My List of Names:
@@ -29,30 +27,23 @@
 #             Charlie
 #     ```
 
-
-
-
 # <---------------------------------------------------------------------------------------------->
-
+print("My List of Names:\n\tAlice\n\tBob\n\tCharlie")
 ## Section B
 # 1. Write code that prints the value of 2 + 2.
-
-
-
+print(2 + 2)
 # 2. Write code that prints the value of 5.7 subtracted from 3.4.
-
-
+sum = 5.7 - 3.4
+print(sum)
 
 # 3. Write code that prints the value of 8 multiplied by 7.
-
-
-
+print(8 * 7)
 # 4. Write code that prints the value of 144 divided by 12.
-
-
-
+result = (144 / 12)
+print(result)
 # 5. Write code that prints the value of the remainder of 67 divided by 12.
-
-
-
+remainder = 67 % 12
+print(remainder)
 # 6. Write code that finds the value of 20 from `4 - 2 * 6 / 3 * 5`. Hint: you might need brackets.
+complex_sum = (4 - 2) * 6 / 3 * 5
+print(complex_sum)  # 20
